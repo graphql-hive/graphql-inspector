ebaffee0bf1ff11dbd83d3b4a630898c68177de3
Fabio Di Peri <fabio.diperi@prima.it>
2026-05-27 17:16:42 +0200

    Minimal update to Node24

--- packages/action/src/action.ts
+++ packages/action/src/action.ts
@@ -1,9 +1,10 @@
 import * as core from '@actions/core';
 import { run } from './run.js';
 
-(global as any).navigator = {
-  userAgent: 'node.js',
-};
+const nodeVersion = process.versions.node;
+if (parseInt(nodeVersion.split('.')[0], 10) < 24) {
+  throw new Error(`Node.js version ${nodeVersion} is not supported. Please use Node.js 24 or higher.`);
+}
 
 run().catch(e => {
   core.setFailed(e.message || e);
