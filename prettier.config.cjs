f5bec3e34e55fb1ca7a128ac92a5fbde349445bb
Dotan Simha <dotansimha@gmail.com>
2026-08-20 13:02:57 +0300

    fix other shit

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const config = require('@theguild/prettier-config');
 
+// eslint-disable-next-line
 config.plugins = config.plugins.map(p => require(p));
 
 module.exports = {
